223e5f1e0ceca96b6015412e05a4280aaa14a381
Mahan <khalili1388mahan@gmail.com>
2025-02-17 18:28:49 +0330

    Update plugin

--- addons/GProgress/Panels/Button Panel/panel.gd
+++ addons/GProgress/Panels/Button Panel/panel.gd
@@ -1,37 +1,38 @@
+@icon("res://addons/GProgress/GPro.svg")
 @tool
 class_name GProgressPanel
 extends Control
 
-@onready var n_tab: TabContainer = $Scroll/Tab
-@onready var n_dialoge_save_path: FileDialog = $SavePath
-@onready var n_dialoge_backup_path: FileDialog = $BackupPath
+@export var n_tab: TabContainer
+@export var n_dialoge_save_path: FileDialog
+@export var n_dialoge_backup_path: FileDialog
 
-@onready var n_user_slots: SpinBox = $"Scroll/Tab/User Settings/user_slots"
-@onready var n_user_parameters: LineEdit = $"Scroll/Tab/User Settings/user_parameters"
-@onready var n_limit_per_user: SpinBox = $"Scroll/Tab/User Settings/limit_per_user"
+@export var n_user_slots: SpinBox
+@export var n_profile_parameters: LineEdit
+@export var n_limit_per_user: SpinBox
 
-@onready var n_progress_parameters: LineEdit = $"Scroll/Tab/Progress Settings/progress_parameters"
-@onready var n_preview_parameters: LineEdit = $"Scroll/Tab/Progress Settings/preview_parameters"
+@export var n_progress_parameters: LineEdit
+@export var n_preview_parameters: LineEdit
 
-@onready var n_autosave_interval: SpinBox = $"Scroll/Tab/Save & Backup/autosave_interval"
-@onready var n_autosave_interval_mode: OptionButton = $"Scroll/Tab/Save & Backup/autosave_interval2"
-@onready var n_save_path: Label = $"Scroll/Tab/Save & Backup/SavePath2"
-@onready var n_save_path_action: Button = $"Scroll/Tab/Save & Backup/save_path"
-@onready var n_backup_interval: SpinBox = $"Scroll/Tab/Save & Backup/backup_interval"
-@onready var n_backup_interval_mode: OptionButton = $"Scroll/Tab/Save & Backup/backup_interval2"
-@onready var n_backup_path: Label = $"Scroll/Tab/Save & Backup/BackupPath2"
-@onready var n_backup_path_action: Button = $"Scroll/Tab/Save & Backup/backup_path"
-@onready var n_compression: CheckBox = $"Scroll/Tab/Save & Backup/compression"
-@onready var n_compression_disable: CheckBox = $"Scroll/Tab/Save & Backup/compression2"
-@onready var n_encryption: CheckBox = $"Scroll/Tab/Save & Backup/encryption"
-@onready var n_encryption_disable: CheckBox = $"Scroll/Tab/Save & Backup/encryption2"
-@onready var n_encryption_key: LineEdit = $"Scroll/Tab/Save & Backup/encryption_key"
+@export var n_autosave_interval: SpinBox
+@export var n_autosave_interval_mode: OptionButton
+@export var n_save_path: Label
+@export var n_save_path_action: Button
+@export var n_backup_interval: SpinBox
+@export var n_backup_interval_mode: OptionButton
+@export var n_backup_path: Label
+@export var n_backup_path_action: Button
+@export var n_compression: CheckBox
+@export var n_compression_disable: CheckBox
+@export var n_encryption: CheckBox
+@export var n_encryption_disable: CheckBox
+@export var n_encryption_key: LineEdit
 
 const CONFIG_FILE = "res://GProgressConfig.txt"
 const CONNECTOR_FILE = "res://addons/GProgress/connector.file"
 
 var config := """user_slots:3
-user_parameters:id, index, name, last_open, last_save
+profile_parameters:id, index, name, last_open, last_save
 limit_per_user:50
 progress_parameters:id, index, name, details, date, time, tags
 preview_parameters:id, index, name, date, time, tags
@@ -49,7 +50,7 @@ var config_dictionary: Dictionary
 
 const config_parameters := [
 	"user_slots",
-	"user_parameters",
+	"profile_parameters",
 	"limit_per_user",
 	"progress_parameters",
 	"preview_parameters",
@@ -87,79 +88,76 @@ func _ready():
 	else:
 		_save_config()
 	config_list = config.split("\n", false)
-	for configure in config_list:
+	for configure: String in config_list:
 		for param in config_parameters:
 			if configure.begins_with(param + ":"):
-				if configure.begins_with("save_path:") or configure.begins_with("backup_path:"):
-					config_dictionary[param] = configure.split(":")[1] + ":" + configure.split(":")[2]
-					continue
-				config_dictionary[param] = configure.split(":")[1]
+				config_dictionary[param] = configure.split(":", false, 1)[1]
 	if config_parameters.size() != config_dictionary.keys().size():
 		printerr("[GProgress] [Config] [GPP] invalid formation in config file export!")
 	_save_dict()
 	update_ui()
 
 func update_ui():
-	n_user_slots.value = int(config_dictionary["user_slots"])
-	n_user_parameters.text = config_dictionary["user_parameters"]
-	n_limit_per_user.value = int(config_dictionary["limit_per_user"])
-	n_progress_parameters.text = config_dictionary["progress_parameters"]
-	n_preview_parameters.text = config_dictionary["preview_parameters"]
-	if config_dictionary["autosave_interval"].ends_with("n"):
+	n_user_slots.value = int(config_dictionary.user_slots)
+	n_profile_parameters.text = config_dictionary.profile_parameters
+	n_limit_per_user.value = int(config_dictionary.limit_per_user)
+	n_progress_parameters.text = config_dictionary.progress_parameters
+	n_preview_parameters.text = config_dictionary.preview_parameters
+	if config_dictionary.autosave_interval.ends_with("n"):
 		n_autosave_interval_mode.selected = 0
 		autosave_mode = AUTOSAVE_NONE
-	elif config_dictionary["autosave_interval"].ends_with("d"):
+	elif config_dictionary.autosave_interval.ends_with("d"):
 		n_autosave_interval_mode.selected = 2
 		autosave_mode = AUTOSAVE_DAY
-	elif config_dictionary["autosave_interval"].ends_with("w"):
+	elif config_dictionary.autosave_interval.ends_with("w"):
 		n_autosave_interval_mode.selected = 3
 		autosave_mode = AUTOSAVE_WEEK
-	elif config_dictionary["autosave_interval"].ends_with("m"):
+	elif config_dictionary.autosave_interval.ends_with("m"):
 		n_autosave_interval_mode.selected = 4
 		autosave_mode = AUTOSAVE_MONTH
-	elif config_dictionary["autosave_interval"].ends_with("y"):
+	elif config_dictionary.autosave_interval.ends_with("y"):
 		n_autosave_interval_mode.selected = 5
 		autosave_mode = AUTOSAVE_YEAR
 	else:
 		printerr("[GProgress] [Config] [GPP] invalid mode for auto-save interval!")
-	n_autosave_interval.value = int(config_dictionary["autosave_interval"].erase(config_dictionary["autosave_interval"].length() - 1))
-	n_save_path.text = config_dictionary["save_path"]
-	if config_dictionary["backup_interval"].ends_with("n"):
+	n_autosave_interval.value = int(config_dictionary.autosave_interval.erase(config_dictionary.autosave_interval.length() - 1))
+	n_save_path.text = config_dictionary.save_path
+	if config_dictionary.backup_interval.ends_with("n"):
 		n_backup_interval_mode.selected = 0
 		backup_mode = BACKUP_NONE
-	elif config_dictionary["backup_interval"].ends_with("d"):
+	elif config_dictionary.backup_interval.ends_with("d"):
 		n_backup_interval_mode.selected = 2
 		backup_mode = BACKUP_DAY
-	elif config_dictionary["backup_interval"].ends_with("w"):
+	elif config_dictionary.backup_interval.ends_with("w"):
 		n_backup_interval_mode.selected = 3
 		backup_mode = BACKUP_WEEK
-	elif config_dictionary["backup_interval"].ends_with("m"):
+	elif config_dictionary.backup_interval.ends_with("m"):
 		n_backup_interval_mode.selected = 4
 		backup_mode = BACKUP_MONTH
-	elif config_dictionary["backup_interval"].ends_with("y"):
+	elif config_dictionary.backup_interval.ends_with("y"):
 		n_backup_interval_mode.selected = 5
 		backup_mode = BACKUP_YEAR
 	else:
 		printerr("[GProgress] [Config] [GPP] invalid mode for backup interval!")
-	n_backup_interval.value = int(config_dictionary["backup_interval"].erase(config_dictionary["backup_interval"].length() - 1))
-	n_backup_path.text = config_dictionary["backup_path"]
-	if bool(int(config_dictionary["compression"])):
+	n_backup_interval.value = int(config_dictionary.backup_interval.erase(config_dictionary.backup_interval.length() - 1))
+	n_backup_path.text = config_dictionary.backup_path
+	if bool(int(config_dictionary.compression)):
 		n_compression.button_pressed = true
 	else:
 		n_compression_disable.button_pressed = true
-	if bool(int(config_dictionary["encryption"])):
+	if bool(int(config_dictionary.encryption)):
 		n_encryption.button_pressed = true
 	else:
 		n_encryption_disable.button_pressed = true
-	n_encryption_key.text = config_dictionary["encryption_key"]
+	n_encryption_key.text = config_dictionary.encryption_key
 
 func update_dict():
-	config_dictionary["user_slots"] = str(n_user_slots.value)
-	config_dictionary["user_parameters"] = n_user_parameters.text
-	config_dictionary["limit_per_user"] = str(n_limit_per_user.value)
-	config_dictionary["progress_parameters"] = n_progress_parameters.text
-	config_dictionary["preview_parameters"] = n_preview_parameters.text
-	config_dictionary["autosave_interval"] = str(n_autosave_interval.value)
+	config_dictionary.user_slots = str(n_user_slots.value)
+	config_dictionary.profile_parameters = n_profile_parameters.text
+	config_dictionary.limit_per_user = str(n_limit_per_user.value)
+	config_dictionary.progress_parameters = n_progress_parameters.text
+	config_dictionary.preview_parameters = n_preview_parameters.text
+	config_dictionary.autosave_interval = str(n_autosave_interval.value)
 	var append = ""
 	match n_autosave_interval_mode.selected:
 		0:
@@ -174,9 +172,9 @@ func update_dict():
 			append = "y"
 		_:
 			printerr("[GProgress] [Config] [GPP] invalid mode for autosave interval!")
-	config_dictionary["autosave_interval"] += append
-	config_dictionary["save_path"] = n_save_path.text
-	config_dictionary["backup_interval"] = str(n_backup_interval.value)
+	config_dictionary.autosave_interval += append
+	config_dictionary.save_path = n_save_path.text
+	config_dictionary.backup_interval = str(n_backup_interval.value)
 	append = ""
 	match n_backup_interval_mode.selected:
 		0:
@@ -191,21 +189,21 @@ func update_dict():
 			append = "y"
 		_:
 			printerr("[GProgress] [Config] [GPP] invalid mode for backup interval!")
-	config_dictionary["backup_interval"] += append
-	config_dictionary["backup_path"] = n_backup_path.text
+	config_dictionary.backup_interval += append
+	config_dictionary.backup_path = n_backup_path.text
 	match n_compression.button_pressed:
 		true:
-			config_dictionary["compression"] = "1"
+			config_dictionary.compression = "1"
 		false:
-			config_dictionary["compression"] = "0"
+			config_dictionary.compression = "0"
 	match n_encryption.button_pressed:
 		true:
-			config_dictionary["encryption"] = "1"
+			config_dictionary.encryption = "1"
 		false:
-			config_dictionary["encryption"] = "0"
-	config_dictionary["encryption_key"] = n_encryption_key.text
-	while config_dictionary["encryption_key"].length() < 32:
-		config_dictionary["encryption_key"] += "?"
+			config_dictionary.encryption = "0"
+	config_dictionary.encryption_key = n_encryption_key.text
+	while config_dictionary.encryption_key.length() < 32:
+		config_dictionary.encryption_key += "?"
 	_save_dict()
 
 
@@ -231,7 +229,7 @@ func _on_open_file_pressed():
 
 func _on_reset_to_defaults_pressed():
 	config = """user_slots:3
-user_parameters:id, index, name, last_open, last_save
+profile_parameters:id, index, name, last_open, last_save
 limit_per_user:50
 progress_parameters:id, index, name, details, date, time, tags
 preview_parameters:id, index, name, date, time, tags
--- addons/GProgress/plugin.gd
+++ addons/GProgress/plugin.gd
@@ -1,16 +1,19 @@
 @tool
 extends EditorPlugin
 
-var panel = preload("res://addons/GProgress/Panels/Button Panel/panel.tscn").instantiate()
+var panel: Control = preload("res://addons/GProgress/Panels/Button Panel/panel.tscn").instantiate()
 
-func _enter_tree():
-	add_control_to_bottom_panel(panel, "GProgress")
+func _enter_tree() -> void:
+	add_control_to_container(EditorPlugin.CONTAINER_PROJECT_SETTING_TAB_RIGHT, panel)
 	add_autoload_singleton("GPro", "res://addons/GProgress/GPro/GProgress.gd")
 
 
-func _exit_tree():
-	remove_control_from_bottom_panel(panel)
+func _exit_tree()-> void :
+	remove_control_from_container(EditorPlugin.CONTAINER_PROJECT_SETTING_TAB_RIGHT, panel)
 	remove_autoload_singleton("GPro")
 
 func _get_plugin_name() -> String:
 	return "GProgress"
+
+func _get_plugin_icon() -> Texture2D:
+	return load("res://addons/GProgress/GPro.svg")
